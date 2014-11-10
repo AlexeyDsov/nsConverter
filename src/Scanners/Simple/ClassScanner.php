0411466edd7230834f24e84ed5a14777066714f9
AlexeyDsov <alexeydsov@gmail.com>
2014-11-10 17:49:19 +0300

    ClassScanner become more lazy

--- src/Scanners/Simple/ClassScanner.php
+++ src/Scanners/Simple/ClassScanner.php
@@ -9,48 +9,62 @@ class ClassScanner implements Scanner
 {
 	public function init(EventEmitter $eventEmitter)
 	{
-		$buffer = false;
-		$inClass = false;
-		$className = null;
-		$penjepitStart = null;
-		$currentPenjepit = null;
-		$classStart = null;
+		$currentPenjepit = 0;
 
-		$classDetector = function ($num, $subject, EventEmitter $ee) use (&$buffer, &$inClass, &$className, &$penjepitStart, &$currentPenjepit, &$classStart) {
-			$buffer = true;
-			$className = '';
-			$inClass = true;
-			$penjepitStart = $currentPenjepit;
-			$classStart = $num;
+		$classDetector = function ($num, $subject, EventEmitter $ee) use (&$currentPenjepit) {
+			$this->registerClassStart($ee, $currentPenjepit, $num);
 		};
-		$inBuffer = function ($num, $subject, EventEmitter $ee) use (&$buffer, &$inClass, &$className, &$penjepitStart, &$currentPenjepit) {
-			if ($buffer) {
-				$className .= $subject[1];
-			}
-		};
-		$bufferEnd = function () use ($eventEmitter, &$buffer, &$inClass, &$className, &$penjepitStart, &$currentPenjepit, &$classStart) {
-			if ($buffer) {
-				$buffer = false;
-				if ($className) {
-					$eventEmitter->emit('classStarted', [$className, $classStart]);
-				}
-			}
-		};
-
-		$penjepiter = function ($count, $num) use (&$buffer, &$inClass, &$className, &$penjepitStart, &$currentPenjepit, &$classStart) {
+		$penjepitCounter = function ($count, $num) use (&$currentPenjepit) {
 			$currentPenjepit = $count;
-			if (!is_null($penjepitStart) && $penjepitStart <= $currentPenjepit) {
-				$inClass = false;
-				$buffer = false;
-				$className = '';
-				$currentPenjepit = null;
-				$classStart = null;
-			}
 		};
 
 		$eventEmitter->on('token:'.T_CLASS, $classDetector);
 		$eventEmitter->on('token:'.T_INTERFACE, $classDetector);
 		$eventEmitter->on('token:'.T_TRAIT, $classDetector);
+		$eventEmitter->on('penjepit', $penjepitCounter);
+	}
+
+	private function registerClassStart(EventEmitter $eventEmitter, $penjepitStart, $classStart)
+	{
+		$className = null;
+		$currentPenjepit = null;
+		$buffer = true;
+
+		$inBuffer = function ($num, $subject, EventEmitter $ee) use (&$className, &$buffer) {
+			if ($buffer) {
+				$className .= $subject[1];
+			}
+		};
+		$bufferEnd = function () use ($inBuffer, &$bufferEnd, $eventEmitter, &$className, &$classStart, &$buffer) {
+			if ($buffer) {
+				$buffer = false;
+				if ($className) {
+					$eventEmitter->emit('classStarted', [$className, $classStart]);
+				}
+				$eventEmitter->removeListener('token:' . T_STRING, $inBuffer);
+				$eventEmitter->removeListener('token:' . T_EXTENDS, $bufferEnd);
+				$eventEmitter->removeListener('token:' . T_IMPLEMENTS, $bufferEnd);
+				$eventEmitter->removeListener('token:{', $bufferEnd);
+				$eventEmitter->removeListener('token:(', $bufferEnd);
+				$eventEmitter->removeListener('token:;', $bufferEnd);
+				$eventEmitter->removeListener('penjepit', $bufferEnd);
+			}
+		};
+		$penjepiter = function ($count, $num) use ($inBuffer, $bufferEnd, &$penjepiter, $eventEmitter, &$penjepitStart, &$currentPenjepit) {
+			$currentPenjepit = $count;
+			if (!is_null($penjepitStart) && $penjepitStart <= $currentPenjepit) {
+				$eventEmitter->removeListener('token:'.T_STRING, $inBuffer);
+				$eventEmitter->removeListener('token:'.T_EXTENDS, $bufferEnd);
+				$eventEmitter->removeListener('token:'.T_IMPLEMENTS, $bufferEnd);
+				$eventEmitter->removeListener('token:{', $bufferEnd);
+				$eventEmitter->removeListener('token:(', $bufferEnd);
+				$eventEmitter->removeListener('token:;', $bufferEnd);
+				$eventEmitter->removeListener('penjepit', $bufferEnd);
+				$eventEmitter->removeListener('penjepit', $penjepiter);
+				$currentPenjepit = null;
+			}
+		};
+
 		$eventEmitter->on('token:'.T_STRING, $inBuffer);
 		$eventEmitter->on('token:'.T_EXTENDS, $bufferEnd);
 		$eventEmitter->on('token:'.T_IMPLEMENTS, $bufferEnd);
@@ -60,37 +74,4 @@ class ClassScanner implements Scanner
 		$eventEmitter->on('penjepit', $bufferEnd);
 		$eventEmitter->on('penjepit', $penjepiter);
 	}
-
-//	public function process($subject, $i)
-//	{
-//		if ($this->penjepitCounter) {
-//			$this->penjepitCounter->process($subject, $i);
-//		}
-//
-//		if (is_array($subject) && in_array($subject[0], [T_CLASS, T_INTERFACE, T_TRAIT])) {
-//			$this->buffer = true;
-//			$this->className = '';
-//			$this->inClass = true;
-//			$this->penjepitCounter = null;
-//		} elseif ($this->buffer) {
-//			$isBufferEnd1 = is_array($subject) && in_array($subject[0], [T_EXTENDS, T_IMPLEMENTS]);
-//			$isBufferEnd2 = is_string($subject) && in_array($subject, ['{', ';', '(']);
-//
-//			if (is_array($subject) && in_array($subject[0], [T_STRING])) {
-//				$this->className .= $subject[1];
-//			} elseif ($isBufferEnd1 || $isBufferEnd2) {
-//				$this->buffer = false;
-//			}
-//		}
-//		if ($this->inClass && !$this->penjepitCounter && is_string($subject) && $subject == '{') {
-//			$this->penjepitCounter = (new PenjepitCounter())->init();
-//			$this->penjepitCounter->process($subject, $i);
-//		}
-//		if ($this->penjepitCounter && !$this->penjepitCounter->isBuffer()) {
-//			$this->inClass = false;
-//			$this->buffer = false;
-//			$this->className = null;
-//			$this->penjepitCounter = null;
-//		}
-//	}
 }
